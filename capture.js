9ec57c1c3f5c3199c021f1c3474a70f47d941667
krasowskiM <krasowski.m87@gmail.com>
2018-03-14 20:50:58 +0100

    Comments

--- capture.js
+++ capture.js
@@ -1,11 +1,28 @@
+/**
+ * Skrypt zawierający logikę streamowania obrazu.
+ */
+
+//inicjalizacja obiektu desktopCapturer odpowiedzialnego za przechwycenie
 const {desktopCapturer} = require('electron');
 
-desktopCapturer.getSources({ types: ['window', 'screen'] }, (error, sources) => {
+// W tej funkcji odbywa się określenie źródła obrazu, więcej info w API
+desktopCapturer.getSources({ 
+    //w tablicy uzupełniamy źródła z których będzie pobierany obraz
+    types: ['window', 'screen'] 
+    //funkcja z parametrami, error pojawia się w przypadku prawdopodobnych problemów I/O
+    //sources - lista/tablica źródeł streamu
+}, (error, sources) => {
     if (error) throw error;
+    //przeszukujemy listę w poszukiwaniu dostępnych źródeł - w tym przypadku będą to wszystkie otwarte
+    //okna oraz cały pulpit
     for (let i = 0; i < sources.length; i++) {
         let name = sources[i].name;
+        //wypisałem na konsolę nazwy wszystkich źródeł -> w aplikacji używamy skrótu Ctrl + Shift + I
+        //przechodzimy do console i powinny tam być wypisanie po kolei źródła obrazu
         console.log(name);
+        //Entire screen jest domyślnie całym dostępnym pulpitem
         if (name == 'Entire screen') {
+            //określenie rozmiarów obrazu oraz przechwycenie wybranego źródła
             navigator.mediaDevices.getUserMedia({
                 audio: false,
                 video: {
@@ -19,19 +36,28 @@ desktopCapturer.getSources({ types: ['window', 'screen'] }, (error, sources) => 
                     }
                 }
             })
+                //Po próbie przechwycenia obrazu możemy przejść do przesyłania go do odbiornika
                 .then((stream) => handleStream(stream))
+                //obsługa w przypadku błędu
                 .catch((e) => handleError(e))
+            //kończymy imprezę
             return;
         }
     }
 });
 
+//podstawowa obsługa streamowania
 function handleStream(stream) {
+    //znajdujemy element odpowiedzialny za wyświetlenie obrazu w aplikacji,
+    //znacznik 'video' z okna głównego
     const video = document.querySelector('video');
+    //określamy obiekt źródłowy dla playera
     video.srcObject = stream;
+    //jeżeli zostały dostarczone metadane, to jedziemy z odtwarzaniem
     video.onloadedmetadata = (e) => video.play();
 }
 
+//podstawowa obsługa błędu, wypisze go tylko na konsolę
 function handleError(e) {
     console.log(e);
 }
--- main.js
+++ main.js
@@ -1,16 +1,34 @@
+/**
+ * Skrypt okna głównego aplikacji. Odpowiada za załadowanie zawartości okna oraz inicjalizację aplikacji
+ * w electronie.
+ */
+
+//Moduły ładowane przez node.js - electron jest tym, co odpala naszą aplikację, url i path ułatwiają odnalezienie plików aplikacji
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+//App jest główną klasą aplikacji electrona, BrowserWindow to domyślne przeglądarkowe "okno główne"
+const { app, BrowserWindow } = electron;
 
+//deklarujemy zmienną dla okna głównego
 let mainWindow;
 
-app.on('ready', function(){
+//odpalamy aplikację
+app.on('ready', function () {
+
+    //Inicjalizujemy okno główne
     mainWindow = new BrowserWindow({});
+    //ładujemy zawartość okna z pliku html, w ten sposób dzieli się też aplikację
+    //na dodatkowe okna np window1, window2 itd...
     mainWindow.loadURL(url.format({
+        //Tutaj załączamy ścieżkę do pliku, w naszym przypadku plik .html ekranu głównego znajduje się
+        //w tym samym folderze. Zmienna path ułatwia odnalezienie relatywnej ścieżki, w razie gdybyśmy opublikowali
+        //pliki na serwerze, __dirname oznacza po prostu zmienną dla aktualnego folderu
         pathname: path.join(__dirname, 'main.html'),
+        //protokół za pomocą którego będziemy odczytywać plik
         protocol: 'file:',
+        //wartość wyznacza, czy slashe '//' po protokole wyżej są wymagane
         slashes: true
     }));
 });
